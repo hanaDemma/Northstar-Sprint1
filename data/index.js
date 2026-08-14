e28e809cd9c69b0b01f9163c42201eb86e3336d3
CJ.. <cjchrys23@gmail.com>
2026-08-14 18:48:57 +0300

    Revert "Add mock stock availability data"

--- data/index.js
+++ data/index.js
@@ -1,6 +1,5 @@
 const orders = require("./orders");
 const returns = require("./returns");
-const products = require("./products");
 
 function findOrder(orderId) {
   return orders.find(order => order.orderId === orderId);
@@ -10,15 +9,9 @@ function findReturn(orderId) {
   return returns.find(item => item.orderId === orderId);
 }
 
-function findProduct(productId) {
-  return products.find(product => product.productId === productId);
-}
-
 module.exports = {
   orders,
   returns,
-  products,
   findOrder,
-  findReturn,
-  findProduct
+  findReturn
 };
